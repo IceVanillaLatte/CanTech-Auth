96e7a1712100cec80ed9bac43595b8b5e42ffb77
yujinKim <yujinkimvv@gmail.com>
2025-02-10 02:32:12 +0900

    test: /test 엔드포인트 단위테스트 추가 (#1)

--- src/main/java/com/cantech/auth/auth_service/deploytest/DeploytestController.java
+++ src/main/java/com/cantech/auth/auth_service/deploytest/DeploytestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DeploytestController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test() {
         return "Auth Server 입니다.";
     }
---
+++ src/test/java/com/cantech/auth/auth_service/test/DeploytestControllerTest.java
@@ -0,0 +1,25 @@
+package com.cantech.auth.auth_service.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(DeploytestControllerTest.class)
+public class DeploytestControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHelloWorld() throws Exception {
+        mockMvc.perform(get("/test"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Auth Server 입니다."));
+    }
+}
